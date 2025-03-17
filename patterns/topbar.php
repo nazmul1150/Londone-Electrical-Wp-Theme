102b6a73ff43a898f0f439f7587ac3f076171f78
nazmul hossen <140499399+nazmul1150@users.noreply.github.com>
2025-03-17 11:39:21 +0600

    update patterns file add topbar

--- patterns/topbar.php
+++ patterns/topbar.php
@@ -8,6 +8,6 @@
 
 <!-- wp:group {"align":"full","backgroundColor":"black","textColor":"white"} -->
 <div class="wp-block-group alignfull has-white-color has-black-background-color has-text-color has-background" style="padding:5px;text-align:center;">
-    <p>📞 Call Us: +123 456 7890 | ✉ Email: <?php echo esc_html('support@example.com'); ?></p>
+    <p>📞 Call Us: <?php echo esc_html('+123 456 7890'); ?> | ✉ Email: <?php echo esc_html('support@example.com'); ?></p>
 </div>
 <!-- /wp:group -->
