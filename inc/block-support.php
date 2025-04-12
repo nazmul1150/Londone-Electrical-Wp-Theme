d402b880092240c28dde24140eff425bb463125c
nazmul hossen <nazmulhossen1150@gmail.com>
2025-04-12 23:21:26 +0600

    update gutenblock

--- inc/block-support.php
+++ inc/block-support.php
@@ -1,10 +1,19 @@
 <?php
-function mytheme_register_blocks() {
-    $blocks = [ 'hero', 'testimonial' ]; // block folder names
+function mytheme_register_gutenberg_blocks() {
+    // Register editor script compiled by wp-scripts
+    wp_register_script(
+        'mytheme-blocks',
+        get_template_directory_uri() . '/build/blocks.js',
+        [ 'wp-blocks', 'wp-element', 'wp-editor', 'wp-components' ],
+        filemtime( get_template_directory() . '/build/blocks.js' )
+    );
 
-    foreach ( $blocks as $block ) {
-        register_block_type( __DIR__ . "/blocks/$block" );
+    // Automatically register all block.json files
+    $blocks = glob( get_template_directory() . '/inc/blocks/*/block.json' );
+
+    foreach ( $blocks as $block_json ) {
+        register_block_type( dirname( $block_json ) );
     }
 }
-add_action( 'init', 'mytheme_register_blocks' );
+add_action( 'init', 'mytheme_register_gutenberg_blocks' );
 
---
+++ inc/blocks/index.js
@@ -0,0 +1,2 @@
+import './home-slider';
+import './gallery';
